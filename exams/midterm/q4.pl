2a1b35734df0c150d71ab9967a896f8d66721adc
Abhijit Balaji <balaabhijit5@gmail.com>
2021-03-23 23:24:39 -0500

    added comments to sudoku

--- exams/midterm/q4.pl
+++ exams/midterm/q4.pl
@@ -7,13 +7,15 @@ range(N,N,[N]).
 select(X, [X|Xs], Xs).
 select(X, [Y|Ys], [Y|Zs]):- select(X,Ys,Zs).
 
-% checks if a list is unique
+% True if a list is unique
 allDiff(L) :- \+ (select(X,L,R), member(X,R)).
 
+% index into 2D list
 index2D(Grid, RowIndex, ColIndex, Element):-
     nth0(RowIndex, Grid, Row),
     nth0(ColIndex, Row, Element).
 
+% Given a column index, get the column vector as a list
 getColVector(Grid, ColIndex, ColVector):-
     index2D(Grid, 0, ColIndex, A),
     index2D(Grid, 1, ColIndex, B),
@@ -21,16 +23,19 @@ getColVector(Grid, ColIndex, ColVector):-
     index2D(Grid, 3, ColIndex, D),
     ColVector = [A, B, C, D].
 
+% generate all possible permutations of the rows
 generateRow(Row):-
     permutation([0,1,2,3], Row).
 
-generateGrid4X4(Grid):-
+% generate all possible grids
+generateGrid2X2(Grid):-
     generateRow(R1),
     generateRow(R2),
     generateRow(R3),
     generateRow(R4),
     Grid = [R1, R2, R3, R4].
 
+% test if the given grid is a valid 2X2 sudoko
 testGrid(Grid):-
     getColVector(Grid, 0, A), allDiff(A),
     getColVector(Grid, 1, B), allDiff(B),
@@ -38,15 +43,18 @@ testGrid(Grid):-
     getColVector(Grid, 3, D), allDiff(D),
     writeGrid(Grid).
 
+% solution using generate test paradigm
 minisudoku:-
-    generateGrid4X4(Grid), testGrid(Grid).
+    generateGrid2X2(Grid), testGrid(Grid).
 
+% writes a given 2D grid to console
 writeGrid([R1, R2, R3, R4|_]):-
     writeRow(R1),
     writeRow(R2),
     writeRow(R3),
     writeRow(R4).
 
+% writes a given row to the console. This is used by write grid
 writeRow([A,B,C,D|_]):-
     write(A), write(" "), write(B), write(" "), write(C), write(" "), write(D), nl.
     
